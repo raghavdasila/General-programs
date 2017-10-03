02432b358da30e19072fbdeae8ed616e67cec62c
Raghavendra Singh Dasila <raghavdasila@outlook.com>
2017-10-03 06:22:29 +0530

    Create predicate.py

---
+++ AIlab/predicate.py
@@ -0,0 +1,25 @@
+import string
+if __name__=="__main__":
+#	pred=raw_input()
+	print "Enter Predicate logic, with each part seperated by space"
+	pred="(( A and not B ) implies C ) and (( not A ) iff ( B and C ))"
+	tokens=pred.split(' ')
+	entities=list(string.ascii_lowercase) + list(string.ascii_uppercase)
+	inputs=[]
+	for t in xrange(len(tokens)):
+		if tokens[t]=='implies':tokens[t]=">="
+		if tokens[t]=='iff':tokens[t]="=="
+		if tokens[t] in entities and tokens[t] not in inputs:
+			inputs.append(tokens[t])
+	n=2**(len(inputs)+1)
+	for i in xrange(0,n):
+		 ins=[int(x) for x in list(bin(i)[2:])]
+		 while len(ins)<len(inputs):
+		 	ins=[0]+ins
+		 print "For inputs:",ins,":",
+		 e_pred=""
+		 for t in tokens:
+		 	if t in inputs:
+		 		t=str(ins[inputs.index(t)])
+		 	e_pred+=t+" "
+		 print int(eval(e_pred))
